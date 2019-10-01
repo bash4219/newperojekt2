6c628adba6d90dd77242cdad880458f0505d1d81
bash4219 <kevin.bash18@gmail.com>
2019-10-01 16:01:26 -0500

    solvint my life peroblems

--- new perojekt2.py
+++ new perojekt2.py
@@ -5,5 +5,13 @@ print("i have 938$ in my bank i need to by 2 moter they ech cost 84 dolers. how 
 # my mony ishus
 print("so naw we need to find how much the 2 moters cost")
 print(84 * 2)
+# haw much the moters cost
 print("so naw we need to tak 168 and subterakt is ferom are totel")
 print(938 - 168)
+# haw much mony i have left
+print("naw i need to find the persent of mony that i took ferom my bank")
+print(938 / 100)
+# 15
+print("we are going to divid the cost of the moters by the persintijs")
+print(168 / 9.38)
+#haw much mony
