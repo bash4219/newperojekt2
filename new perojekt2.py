8999918e060dcb01d7988cd6742e1f77c8c4ecca
bash4219 <kevin.bash18@gmail.com>
2019-10-02 15:53:22 -0500

    solvint my life peroblems

--- new perojekt2.py
+++ new perojekt2.py
@@ -11,7 +11,27 @@ print(938 - 168)
 # haw much mony i have left
 print("naw i need to find the persent of mony that i took ferom my bank")
 print(938 / 100)
-# 15
+# 1% of the totel number
 print("we are going to divid the cost of the moters by the persintijs")
 print(168 / 9.38)
-#haw much mony
+# wot persent the moter sosted
+print("this is the anser")
+print(100 - 17.91044776119403)
+
+
+# vaereubls
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_drivers = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+
+print("thaer are", cars,"cars averij")
+print("thaer are only", drivers,"derivers avalibel")
+print("thaer will be", cars_not_drivers, "emty cars today")
+print("we can teribut", carpool_capacity,"pepel to day")
+print("we have", passengers, "to car pool to day")
+print("we have to pool abawt", average_passengers_per_car, "in ech car")
